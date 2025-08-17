16f46a9d3ed38cf0ccb38b9897e3e68c918b9d88
José Lucas <joselucas.dev@gmail.com>
2025-08-17 11:37:54 -0300

    feat: add support cors.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import router from './routes/router';
 import { errorHandler } from './exceptions/GlobalHandlerException';
+import cors from 'cors';
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors());
 
 app.use(router);
 
